74c9ecb33dc99e038b53bc37e24a5914f93f6d0e
tobiwin <tobiwin@protonmail.com>
2026-09-21 14:57:56 +0200

    Update backup script to use /bin/sh as entrypoint for container backup

--- scripts/paperless-backup.sh
+++ scripts/paperless-backup.sh
@@ -4,4 +4,5 @@ set -eu
 PROJECT_DIR=$(CDPATH= cd -- "$(dirname -- "$0")/.." && pwd)
 
 cd "$PROJECT_DIR"
-exec docker compose run --rm --entrypoint /usr/local/bin/paperless-container-backup backup
+exec docker compose run --rm --entrypoint /bin/sh backup \
+	/usr/local/bin/paperless-container-backup
